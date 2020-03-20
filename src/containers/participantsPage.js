7d10fa78bddafcd0f8662247d2fb73bdd40693d2
ana paula <anapaulamak@gmail.com>
2020-03-20 00:17:03 -0300

    Create participants page

---
+++ src/containers/participantsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function ParticipantsPage(){
+  return(
+    <>
+      <h1>Lista de Participantes</h1>
+      <label>Nome</label>
+      <input></input>
+
+      <label>Email</label>
+      <input></input>
+
+      <label>Telefone</label>
+      <input></input>
+    </>
+  )
+}
