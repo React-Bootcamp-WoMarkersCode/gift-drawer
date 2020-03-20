c0944ae92b44c7740fcb85137d8b463bd5698b3c
Diana Coelho <coelhodiana04@gmail.com>
2020-03-20 10:31:38 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GiftList from './components/GiftList'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <GiftList />
     </div>
   );
 }
---
+++ src/components/GiftList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GiftList = () => {
+    return ( 
+        <>\
+            <h1>Lista de Brindes</h1>
+            <p>Brinde 1</p>
+        </>
+     );
+}
+ 
+export default GiftList;
