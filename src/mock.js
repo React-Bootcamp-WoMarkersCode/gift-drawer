6c82daa0c6d300122a3a5f67b2ac6b7112fc104e
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-03-21 11:23:37 -0300

    Mock.js

---
+++ src/mock.js
@@ -0,0 +1,33 @@
+export const List = [
+    {
+        "name": 'Ana',
+        "email": 'ana@gmail.com',
+        "phone": 91111-1111
+    },
+    {
+        "name": 'Bia',
+        "email": 'bia@gmail.com',
+        "phone": 91111-1111
+    },
+    {
+        "name": 'Caio',
+        "email": 'caio@gmail.com',
+        "phone": 91111-1111
+    },
+    {
+        "name": 'Ana',
+        "email": 'ana@gmail.com',
+        "phone": 91111-1111
+    },
+    {
+        "name": 'Bia',
+        "email": 'bia@gmail.com',
+        "phone": 91111-1111
+    },
+    {
+        "name": 'Caio',
+        "email": 'caio@gmail.com',
+        "phone": 91111-1111
+    },
+
+]
